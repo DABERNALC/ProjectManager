f6daa5d23bf96ed0a199b8acbd5440c251130894
Isaac_Herrera <isaacheye@unisabana.edu.co>
2021-09-09 09:14:08 -0500

    initial implementation of creating a team

--- API/router/teams.js
+++ API/router/teams.js
@@ -3,6 +3,7 @@
 */
 const { Router } = require('express');
 const { check }  = require('express-validator');
+const { validate } = require('../middlewares/validate');
 
 // import  Controlers
 
@@ -37,6 +38,27 @@ router.get('/test',(req,res)=>{
         res.status(500).send("compa su request tiene que ser de más de 1 palabra");
     }
     
+});
+
+router.get('/create',[
+    check("liderId","you should send an leader id to create a team").not().isEmpty(),
+    check("teamName","you should send the team name to create a team").not().isEmpty()
+    , validate
+],(req,res)=>{
+        //getting the parameters from the request
+        const liderId = req.query.liderId;
+        const teamName = req.query.teamName;
+
+        
+        //logic to create a team
+
+        //todo: llamar a la base de datos
+        
+        //regresar respuesta
+        res.json({
+            status:"ok",
+            message:`i created the team "${teamName}""`
+        });
 });
 
 
