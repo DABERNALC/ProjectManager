061357685eb638978fd0e22c062ab6205ea48bad
NicolasCaMe <62396717+NicolasCaMe@users.noreply.github.com>
2021-10-27 11:43:37 -0500

    adding react-router to the project

--- Cliente/project_manager/src/App.js
+++ Cliente/project_manager/src/App.js
@@ -1,4 +1,4 @@
-import AppStyle from'./App.css';
+import AppStyle from './App.css';
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import Header from './Components/Header/Header';
 import SideBar from "./Components/SideBar/SideBar"
@@ -7,20 +7,47 @@ import Kanban from './Pages/Kanban/Kanban';
 import Projects from "./Pages/Projects/Projects"
 import Teams from './Pages/Teams/Teams';
 import LogIn from './Pages/LogIn/LogIn';
+import { BrowserRouter, Route } from "react-router-dom"
 function App() {
   return (
     <div>
-      <div className="App">
-      <Header></Header>
-      <SideBar></SideBar>
-      <Teams></Teams>
-      {/* <Kanban/> */}
-      {/* <LogIn/> */}
+      {/*
       
-      
+      / muestra la landing page
+      /signIn es el formulario para entrar a una cuenta
 
-      </div>
-      {/* <LandingPage/> */}
+      
+      /app/etc muestra el header y el sidebar
+      /app/teams muestra la vista de todos los equipos
+      
+      
+      */}
+
+      <BrowserRouter>
+        <div className="App">
+          <Route path="/" exact >
+            <LandingPage />
+          </Route>
+          <Route path="/signIn" exact >
+            <LogIn/>
+          </Route>
+          <Route path="/app"  >
+            <Header></Header>
+            <SideBar></SideBar>
+          </Route>
+          <Route path="/app/teams" exact >
+            <Teams/>
+          </Route>
+          <Route path="/app/project" exact >
+            <Kanban/>
+          </Route>
+        </div>
+
+
+      </BrowserRouter>
+
+
+
     </div>
   );
 }
